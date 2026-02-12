ff1760cb3eaf7b8f4cb4e8b798aaaf065c73c182
Arthur <arthur.pradier81@gmail.com>
2026-02-12 11:01:12 +0100

    feature(DoF): better DoF!!
    I'm still doing some very bad down/upscaling, but thanks GPU zen!

--- src/Lumiere/Renderer/Passes/DepthOfField.cpp
+++ src/Lumiere/Renderer/Passes/DepthOfField.cpp
@@ -57,6 +57,8 @@ DepthOfField::DepthOfField(uint32_t width, uint32_t height)
     ResourcesManager::Instance()->CreateTexture(DOF_TEMP_BLUR_NAME, blurDesc);
     ResourcesManager::Instance()->CreateTexture(DOF_NEAR_BLUR_NAME, blurDesc);
     ResourcesManager::Instance()->CreateTexture(DOF_FAR_BLUR_NAME, blurDesc);
+    ResourcesManager::Instance()->CreateTexture(DOF_NEAR_FILL_NAME, blurDesc);
+    ResourcesManager::Instance()->CreateTexture(DOF_FAR_FILL_NAME, blurDesc);
     ResourcesManager::Instance()->CreateTexture(DOF_NAME, finalDesc);
 
     std::vector<gpu::Shader::ShaderSource> sources =
@@ -65,17 +67,29 @@ DepthOfField::DepthOfField(uint32_t width, uint32_t height)
     };
     ResourcesManager::Instance()->CacheShader(DOF_PLANE_SEPARATION_SHADER_NAME, sources);
 
-    std::vector<gpu::Shader::ShaderSource> blurNearSources =
+    std::vector<gpu::Shader::ShaderSource> nearExpandSources =
     {
-        {gpu::Shader::Compute, "shaders/dof_blur_near.comp"}
+        {gpu::Shader::Compute, "shaders/dof_expand_near.comp"}
     };
-    ResourcesManager::Instance()->CacheShader(DOF_NEAR_BLUR_SHADER_NAME, blurNearSources);
+    ResourcesManager::Instance()->CacheShader(DOF_NEAR_EXPAND_SHADER_NAME, nearExpandSources);
 
-    std::vector<gpu::Shader::ShaderSource> blurFarSources =
+    std::vector<gpu::Shader::ShaderSource> expandBlurSources =
     {
-        {gpu::Shader::Compute, "shaders/dof_blur_far.comp"}
+        {gpu::Shader::Compute, "shaders/dof_box_blur.comp"}
     };
-    ResourcesManager::Instance()->CacheShader(DOF_FAR_BLUR_SHADER_NAME, blurFarSources);
+    ResourcesManager::Instance()->CacheShader(DOF_NEAR_EXPAND_BLUR_SHADER_NAME, expandBlurSources);
+
+    std::vector<gpu::Shader::ShaderSource> bokehBlurSources =
+    {
+        {gpu::Shader::Compute, "shaders/dof_bokeh_blur.comp"}
+    };
+    ResourcesManager::Instance()->CacheShader(DOF_BOKEH_BLUR_SHADER_NAME, bokehBlurSources);
+
+    std::vector<gpu::Shader::ShaderSource> bokehFillSources =
+    {
+        {gpu::Shader::Compute, "shaders/dof_bokeh_fill.comp"}
+    };
+    ResourcesManager::Instance()->CacheShader(DOF_BOKEH_FILL_SHADER_NAME, bokehFillSources);
 
     std::vector<gpu::Shader::ShaderSource> compositeSources =
     {
@@ -96,6 +110,7 @@ void DepthOfField::Render(const SceneDesc &scene)
     gpu::TexturePtr depth = ResourcesManager::Instance()->GetTexture(GBuffer::GBUFFER_DEPTH_NAME);
     gpu::TexturePtr frame = ResourcesManager::Instance()->GetTexture(Bloom::BLOOM_NAME);
     gpu::TexturePtr separation = ResourcesManager::Instance()->GetTexture(DOF_SEPARATION_NAME);
+    gpu::TexturePtr tempBlur = ResourcesManager::Instance()->GetTexture(DOF_TEMP_BLUR_NAME);
 
     depth->Bind(0);
     separation->BindImage(1, 0, gpu::GLUtils::Write);
@@ -103,50 +118,87 @@ void DepthOfField::Render(const SceneDesc &scene)
     separationShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
     separationShader->Wait();
 
-    // blur the near field in the 2 axis
-    gpu::ShaderPtr blurNearShader = ResourcesManager::Instance()->GetShader(DOF_NEAR_BLUR_SHADER_NAME);
-    blurNearShader->Bind();
-    blurNearShader->UniformData("direction", 0);
+    // expand the near field to have a smooth transition between near and far
+    if (m_expansionSize > 0 && m_expansionBlur > 0)
+    {
+        gpu::ShaderPtr expandShader = ResourcesManager::Instance()->GetShader(DOF_NEAR_EXPAND_SHADER_NAME);
+        expandShader->Bind();
 
-    gpu::TexturePtr tempBlur = ResourcesManager::Instance()->GetTexture(DOF_TEMP_BLUR_NAME);
+        separation->Bind(0);
+        tempBlur->BindImage(1, 0, gpu::GLUtils::Write);
+
+        expandShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
+        expandShader->Wait();
+
+        // blur the near field for a smooth transition between near and in focus
+        gpu::ShaderPtr expandBlurShader = ResourcesManager::Instance()->GetShader(DOF_NEAR_EXPAND_BLUR_SHADER_NAME);
+        expandBlurShader->Bind();
+
+        tempBlur->Bind(0);
+        separation->BindImage(1, 0, gpu::GLUtils::Write);
+
+        expandBlurShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
+        expandBlurShader->Wait();
+    }
+
+    // apply a bokeh blur
+    gpu::ShaderPtr bokehShader = ResourcesManager::Instance()->GetShader(DOF_BOKEH_BLUR_SHADER_NAME);
+    bokehShader->Bind();
+
     gpu::TexturePtr nearBlur = ResourcesManager::Instance()->GetTexture(DOF_NEAR_BLUR_NAME);
-
-    frame->Bind(0);
-    blurNearShader->UniformData("frame", 0);
-    separation->Bind(1);
-    blurNearShader->UniformData("coc", 1);
-    tempBlur->BindImage(2, 0, gpu::GLUtils::Write);
-
-    blurNearShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
-    blurNearShader->Wait();
-
-    blurNearShader->UniformData("direction", 1);
-    tempBlur->Bind(0);
-    nearBlur->BindImage(2, 0, gpu::GLUtils::ReadWrite);
-    blurNearShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
-    blurNearShader->Wait();
-
-    // blur the far field in the 2 axis
-    gpu::ShaderPtr blurFarShader = ResourcesManager::Instance()->GetShader(DOF_FAR_BLUR_SHADER_NAME);
-    blurFarShader->Bind();
-    blurFarShader->UniformData("direction", 0);
-
     gpu::TexturePtr farBlur = ResourcesManager::Instance()->GetTexture(DOF_FAR_BLUR_NAME);
 
     frame->Bind(0);
-    blurFarShader->UniformData("frame", 0);
+    bokehShader->UniformData("frame", 0);
     separation->Bind(1);
-    blurFarShader->UniformData("coc", 1);
-    tempBlur->BindImage(2, 0, gpu::GLUtils::Write);
+    bokehShader->UniformData("coc", 1);
+    nearBlur->BindImage(2, 0, gpu::GLUtils::Write);
+    farBlur->BindImage(3, 0, gpu::GLUtils::Write);
 
-    blurFarShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
-    blurFarShader->Wait();
+    bokehShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
+    bokehShader->Wait();
 
-    blurFarShader->UniformData("direction", 1);
-    tempBlur->Bind(0);
-    farBlur->BindImage(2, 0, gpu::GLUtils::ReadWrite);
-    blurFarShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
-    blurFarShader->Wait();
+    // our bokeh blur pattern leaves holes inbetween its texels, so we must fill it
+    gpu::ShaderPtr bokehFillShader = ResourcesManager::Instance()->GetShader(DOF_BOKEH_FILL_SHADER_NAME);
+    bokehFillShader->Bind();
+
+    gpu::TexturePtr nearFill = ResourcesManager::Instance()->GetTexture(DOF_NEAR_FILL_NAME);
+    gpu::TexturePtr farFill = ResourcesManager::Instance()->GetTexture(DOF_FAR_FILL_NAME);
+
+    separation->Bind(0);
+    nearBlur->BindImage(1, 0, gpu::GLUtils::Read);
+    farBlur->BindImage(2, 0, gpu::GLUtils::Read);
+    nearFill->BindImage(3, 0, gpu::GLUtils::Write);
+    farFill->BindImage(4, 0, gpu::GLUtils::Write);
+
+    bokehFillShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
+    bokehFillShader->Wait();
+
+    // blurNearShader->UniformData("direction", 1);
+    // tempBlur->Bind(0);
+    // nearBlur->BindImage(2, 0, gpu::GLUtils::ReadWrite);
+    // blurNearShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
+    // blurNearShader->Wait();
+
+    // blur the far field in the 2 axis
+    // gpu::ShaderPtr blurFarShader = ResourcesManager::Instance()->GetShader(DOF_FAR_BLUR_SHADER_NAME);
+    // blurFarShader->Bind();
+    // blurFarShader->UniformData("direction", 0);
+    //
+    // frame->Bind(0);
+    // blurFarShader->UniformData("frame", 0);
+    // separation->Bind(1);
+    // blurFarShader->UniformData("coc", 1);
+    // tempBlur->BindImage(2, 0, gpu::GLUtils::Write);
+    //
+    // blurFarShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
+    // blurFarShader->Wait();
+    //
+    // blurFarShader->UniformData("direction", 1);
+    // tempBlur->Bind(0);
+    // farBlur->BindImage(2, 0, gpu::GLUtils::ReadWrite);
+    // blurFarShader->Dispatch(std::ceil(m_intermediateWidth / 16) + 1, std::ceil(m_intermediateHeight / 16) + 1, 1);
+    // blurFarShader->Wait();
 
     // composite into the final result
     gpu::ShaderPtr compositeShader = ResourcesManager::Instance()->GetShader(DOF_COMPOSITE_SHADER_NAME);
@@ -156,9 +208,9 @@ void DepthOfField::Render(const SceneDesc &scene)
 
     separation->Bind(0);
     compositeShader->UniformData("coc", 0);
-    nearBlur->Bind(1);
+    nearFill->Bind(1);
     compositeShader->UniformData("nearBlur", 1);
-    farBlur->Bind(2);
+    farFill->Bind(2);
     compositeShader->UniformData("farBlur", 2);
     frame->Bind(3);
     compositeShader->UniformData("frame", 3);
@@ -174,6 +226,8 @@ void DepthOfField::RenderUI()
     {
         ImGui::InputFloat("Focus distance", &m_focusDistance);
         ImGui::InputFloat("Focus range", &m_focusRange);
+        ImGui::SliderInt("Expansion Size", &m_expansionSize, 0, 10);
+        ImGui::SliderInt("Expansion Blur", &m_expansionBlur, 0, 10);
         if (ImGui::CollapsingHeader("Preview"))
         {
             ImGui::Text("Separation");
@@ -211,6 +265,14 @@ void DepthOfField::Rebuild(uint32_t width, uint32_t height)
     gpu::TexturePtr farBlur = ResourcesManager::Instance()->GetTexture(DOF_FAR_BLUR_NAME);
     farBlur->SetSize(static_cast<int>(m_intermediateWidth), static_cast<int>(m_intermediateHeight));
     farBlur->Reallocate();
+
+    gpu::TexturePtr nearFill = ResourcesManager::Instance()->GetTexture(DOF_NEAR_FILL_NAME);
+    nearFill->SetSize(static_cast<int>(m_intermediateWidth), static_cast<int>(m_intermediateHeight));
+    nearFill->Reallocate();
+
+    gpu::TexturePtr farFill = ResourcesManager::Instance()->GetTexture(DOF_FAR_FILL_NAME);
+    farFill->SetSize(static_cast<int>(m_intermediateWidth), static_cast<int>(m_intermediateHeight));
+    farFill->Reallocate();
 
     gpu::TexturePtr dof = ResourcesManager::Instance()->GetTexture(DOF_NAME);
     dof->SetSize(static_cast<int>(m_width), static_cast<int>(m_height));
--- src/Lumiere/Renderer/Passes/DepthOfField.h
+++ src/Lumiere/Renderer/Passes/DepthOfField.h
@@ -16,19 +16,25 @@ private:
     uint32_t m_intermediateWidth;
     uint32_t m_intermediateHeight;
 
-    float m_focusDistance = 50;
-    float m_focusRange = 10;
+    float m_focusDistance = 2;
+    float m_focusRange = 5;
+    int m_expansionSize = 5;
+    int m_expansionBlur = 5;
 public:
     #pragma region Constants
     constexpr static const char* DOF_SEPARATION_NAME = "DOF_SEPARATION";
     constexpr static const char* DOF_TEMP_BLUR_NAME = "DOF_TEMP_BLUR";
     constexpr static const char* DOF_NEAR_BLUR_NAME = "DOF_NEAR_BLUR";
     constexpr static const char* DOF_FAR_BLUR_NAME = "DOF_FAR_BLUR";
+    constexpr static const char* DOF_NEAR_FILL_NAME = "DOF_NEAR_FILL";
+    constexpr static const char* DOF_FAR_FILL_NAME = "DOF_FAR_FILL";
     constexpr static const char* DOF_NAME = "DOF";
 
     constexpr static const char* DOF_PLANE_SEPARATION_SHADER_NAME = "DOF_PLANE_SEPARATION_SHADER";
-    constexpr static const char* DOF_NEAR_BLUR_SHADER_NAME = "DOF_NEAR_BLUR_SHADER";
-    constexpr static const char* DOF_FAR_BLUR_SHADER_NAME = "DOF_FAR_BLUR_SHADER";
+    constexpr static const char* DOF_NEAR_EXPAND_SHADER_NAME = "DOF_NEAR_EXPAND_SHADER";
+    constexpr static const char* DOF_NEAR_EXPAND_BLUR_SHADER_NAME = "DOF_NEAR_EXPAND_BLUR_SHADER";
+    constexpr static const char* DOF_BOKEH_BLUR_SHADER_NAME = "DOF_BOKEH_BLUR_SHADER";
+    constexpr static const char* DOF_BOKEH_FILL_SHADER_NAME = "DOF_BOKEH_FILL_SHADER";
     constexpr static const char* DOF_COMPOSITE_SHADER_NAME = "DOF_COMPOSITE_SHADER";
     #pragma endregion Constants
 
