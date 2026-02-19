18a60827d59bb55297f8bff8e3acb5d9bfb307ce
Belle <bellpradier@proton.me>
2026-02-19 14:25:43 +0100

    feature(Pipeline): Name + serialize/deserialize
    Pipelines arecreated with a name and can serialize or deserialized themselves, as well as all the render passes they hold.

--- src/Lumiere/Renderer/RenderPipeline.cpp
+++ src/Lumiere/Renderer/RenderPipeline.cpp
@@ -4,14 +4,19 @@
 
 #include "RenderPipeline.h"
 
+#include <fstream>
 #include <memory>
 
+#include <yaml-cpp/node/node.h>
+#include <yaml-cpp/yaml.h>
 #include "Lumiere/ResourcesManager.h"
 #include "Lumiere/Events/RenderEvents.h"
+#include "Passes/PassFactory.h"
 
 namespace lum::rdr {
-RenderPipeline::RenderPipeline(const std::shared_ptr<evt::EventHandler>& handler)
-    : m_eventHandler(handler)
+RenderPipeline::RenderPipeline(const std::string& name, const std::shared_ptr<evt::EventHandler>& handler)
+    : m_name(name)
+    , m_eventHandler(handler)
 {
     m_cameraData = std::make_unique<lum::gpu::Buffer>(lum::gpu::Buffer::BufferType::Uniform);
     LUM_SUB_TO_EVENT(m_eventHandler, evt::EventType::FramebufferResized, RenderPipeline::OnEvent);
@@ -44,6 +49,43 @@ void RenderPipeline::RenderUI()
 void RenderPipeline::AddPass(const std::shared_ptr<IPass>& pass)
 {
     m_passes.push_back(pass);
+}
+
+void RenderPipeline::Serialize() const
+{
+    YAML::Node root;
+    root["version"] = SERIALIZER_VERSION;
+    root["name"] = m_name;
+    YAML::Node passesNode = root["passes"];
+
+    for (const auto& pass : m_passes)
+    {
+        pass->Serialize(passesNode);
+    }
+
+    std::ofstream out("config/" + m_name + ".yaml");
+    out << root;
+}
+
+void RenderPipeline::Deserialize(const YAML::Node &pipeline)
+{
+    if (pipeline["version"].as<int>() != SERIALIZER_VERSION)
+    {
+        std::cerr << "Serializer version mismatch. This pipeline uses deprecated serialization standards.\n";
+        return;
+    }
+
+    // parse all passes and create the appropriate IPass implementation
+    for (const auto& pass : pipeline["passes"])
+    {
+        std::unique_ptr<IPass> constructed = PassFactory::Create(pass["name"].as<std::string>());
+        if (constructed != nullptr)
+        {
+            std::cerr << "Deserialized pass " << pass["name"].as<std::string>() << "\n";
+            constructed->Deserialize(static_cast<YAML::Node>(pass));
+            m_passes.push_back(std::move(constructed));
+        }
+    }
 }
 
 void RenderPipeline::Rebuild(const glm::vec2 &newSize)
--- src/Lumiere/Renderer/RenderPipeline.h
+++ src/Lumiere/Renderer/RenderPipeline.h
@@ -16,12 +16,16 @@ namespace lum::rdr
 class RenderPipeline
 {
 private:
+    constexpr static int SERIALIZER_VERSION = 1;
+
     struct CameraData
     {
         glm::mat4 viewMatrix;
         glm::mat4 projectionMatrix;
         glm::vec3 position;
     };
+
+    std::string m_name;
 
     std::vector<std::shared_ptr<IPass>> m_passes;
     std::unique_ptr<gpu::Buffer> m_cameraData;
@@ -30,10 +34,21 @@ public:
     constexpr static const char* RENDERED_FRAME_NAME = "RENDERED_FRAME";
     constexpr static ImVec2 DEBUG_DISPLAY_SIZE = ImVec2(200.f, 200.f);
 
-    RenderPipeline(const std::shared_ptr<evt::EventHandler>& handler);
+    RenderPipeline(const std::string& name, const std::shared_ptr<evt::EventHandler>& handler);
     void Render(const FrameData &frameData) const;
     void RenderUI();
     void AddPass(const std::shared_ptr<IPass>& pass);
+
+    /**
+     * \brief Export the render pipeline and all its passes to a yaml file
+     */
+    void Serialize() const;
+
+    /**
+     * \brief Read a YAML node containing the pipeline description and deserializes all associated passes
+     * \param pipeline a YAML node containing the pipeline description
+     */
+    void Deserialize(const YAML::Node &pipeline);
 
     /**
      * \brief Rebuild the pipeline to account for a new final resolution size
