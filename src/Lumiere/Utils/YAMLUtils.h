68e8206d78d39c00c0ef865b3868cf42b7627bbe
Belle <bellpradier@proton.me>
2026-03-20 12:09:47 +0100

    feature(Serialize): Quaternion serialization
    Mandatory after last change

--- src/Lumiere/Utils/YAMLUtils.h
+++ src/Lumiere/Utils/YAMLUtils.h
@@ -6,6 +6,7 @@
 
 #include <yaml-cpp/yaml.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 namespace YAML
 {
@@ -29,6 +30,32 @@ struct convert<glm::vec3>
         rhs.x = node[0].as<float>();
         rhs.y = node[1].as<float>();
         rhs.z = node[2].as<float>();
+        return true;
+    }
+};
+
+template<>
+struct convert<glm::quat>
+{
+    static Node encode(const glm::quat& q)
+    {
+        Node node;
+        node.push_back(q.x);
+        node.push_back(q.y);
+        node.push_back(q.z);
+        node.push_back(q.w);
+        return node;
+    }
+
+    static bool decode(const Node& node, glm::quat& rhs) {
+        if(!node.IsSequence() || node.size() != 4) {
+            return false;
+        }
+
+        rhs.x = node[0].as<float>();
+        rhs.y = node[1].as<float>();
+        rhs.z = node[2].as<float>();
+        rhs.w = node[3].as<float>();
         return true;
     }
 };
